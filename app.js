c9f1dd81f27225373abf03a4747206d11801d904
Frank <fmeza07@gmail.com>
2015-02-25 10:42:22 -0800

    add ability to add and display courses

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 var courseRoster = angular.module('courseRoster', ['ui.router']);
+
+courseRoster.config(function($stateProvider) {
+  $stateProvider.state('home', {
+    url:"",
+    templateUrl: "partials/home.html"
+  });
+
+  $stateProvider.state('courses', {
+    url: "/courses",
+    templateUrl: "partials/courses.html",
+    controller: 'CoursesCtrl'
+  });
+});
