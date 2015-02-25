7299269a8f3150c11652048a4258d48227ddfd29
Frank <benjaminherson@gmail.com>
2015-02-25 12:05:46 -0800

    remove debugging tool

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -1,7 +1,5 @@
 courseRoster.controller('StudentsCtrl', function StudentsCtrl($scope, $stateParams, CoursesFactory, UtilitiesFactory) {
   $scope.course = UtilitiesFactory.findById(CoursesFactory.courses, $stateParams.courseId);
-  console.log($stateParams.courseId);
-  console.log($scope.course);
   $scope.addStudent = function() {
     $scope.course.students.push({ name: $scope.studentName});
     $scope.studentName = null;
